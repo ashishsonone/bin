e021ad0d87d56e53f8cb2a00e9c4af9deeee1942
Ashish Sonone <ashson07@gmail.com>
2015-11-27 01:27:56 +0530

    battery monitor - project to detech how battery peforms - event triggered

---
+++ battery_monitor.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+<<suspend
+/etc/pm/sleep.d, /usr/lib/pm-utils/sleep.d
+Programs in these directories (called hooks) are combined
+and executed in C sort order before suspend and hibernate
+with as argument 'suspend' or 'hibernate'. Afterwards they
+are called in reverse order with argument ´resume´ and
+'thaw' respectively. 
+suspend
+
+<<power
+When you plug in/out the AC adapter, the scripts in /etc/pm/power.d
+power
+
+date=$(/bin/date)
+percentage=$(/usr/bin/acpi -V | /usr/bin/head -n 1 | /usr/bin/awk -F ',' '{print $2}')
+
+if [ "$1" == "suspend" ]
+then
+    echo "suspend " $date $percentage>> /home/ashish/Desktop/battery.txt
+fi
+
+if [ "$1" == "resume" ]
+then
+    echo "resume " $date $percentage>> /home/ashish/Desktop/battery.txt
+fi
+
