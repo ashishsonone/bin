63523d1f69f7f00e6d983eae15120658b27ae101
Ashish Sonone <ashson07@gmail.com>
2017-07-02 13:12:05 +0530

    google cloud utilities

---
+++ google-cloud/gce_ip.sh
@@ -0,0 +1,9 @@
+#aws ec2 ssh
+#ssh -i ~/Documents/knit.pem ubuntu@ec2-52-26-56-243.us-west-2.compute.amazonaws.com
+
+#gce loadbalancer ip
+export lb=107.178.241.103:443
+export slave=107.167.179.249
+export master=104.199.158.193
+export aslave=ashish@107.167.179.249
+export amaster=ashish@104.199.158.193
---
+++ google-cloud/gce_ssh.sh
@@ -0,0 +1,9 @@
+alias gssh-cm="gcloud compute ssh manager --zone asia-east1-c"
+
+alias gssh-dn1="gcloud compute ssh dev-node-1"
+alias gssh-dm1="gcloud compute ssh dev-mongo-1"
+
+alias gssh-prod-pn1="gcloud compute ssh prod-node-1 --zone asia-east1-a"
+
+alias gssh-prod-pm1="gcloud compute ssh prod-mongo-1 --zone asia-east1-a"
+alias gssh-prod-pm2-sec="gcloud compute ssh prod-mongo-2-secondary --zone us-central1-a"
