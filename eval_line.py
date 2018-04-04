fed0f5c16c427763aa0197c57961c52d198f26b0
Ashish Sonone <ashson07@gmail.com>
2018-04-04 11:00:41 +0530

    document eval_line

--- eval_line.py
+++ eval_line.py
@@ -1,5 +1,9 @@
 import sys
 import re
+if len(sys.argv) < 2:
+    print('Usage')
+    print("\tpython eval_line.py '- 1.7 (groc 3 apr) - 3 (?? furniture) - 21 (rent)'")
+    sys.exit(0)
 expr = sys.argv[1]
 print(expr)
 
