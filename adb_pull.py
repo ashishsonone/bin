a567159e783753c7f798387dcfad2e11123c826f
Ashish Sonone <ashson07@gmail.com>
2018-06-05 12:54:47 +0530

    useful adb pull script to pull files matching a given regex pattern. e.g 'Screenshot_2018-06-05-.*.png

---
+++ adb_pull.py
@@ -0,0 +1,36 @@
+import sys
+import os
+
+if len(sys.argv) < 3:
+    print('Usage')
+    print("\tpython adb_pull.py <android-src> <regex-pattern> <pc-dest>")
+    print("\tpython adb_pull.py '/sdcard/' '^x.*json' /home/ashish/Downloads")
+    print("\tpython adb_pull.py '/sdcard/DCIM/Screenshots/' 'Screenshot_2018-06-05-.*.png' ~/ssd/Downloads/sc/")
+    sys.exit(0)
+
+src = sys.argv[1]
+pattern = sys.argv[2]
+dest = sys.argv[3]
+
+print(src, pattern, dest)
+
+if src[-1] != '/':
+  src = src + '/'
+
+command = "adb shell ls '{}' | grep -e '{}'".format(src, pattern)
+print(command)
+files = os.popen(command).read()
+files = files.split('\n')
+
+success = 0
+for fname in files:
+  fname = fname.strip()
+  if not fname:
+    continue
+
+  command = "adb pull '{}{}' '{}'".format(src, fname, dest)
+  print(command)
+  os.popen(command).read()
+  success += 1
+
+print('Finished pulling {} files'.format(success))
